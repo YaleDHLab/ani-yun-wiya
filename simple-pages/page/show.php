72c590af5d9683c82a6019a00940b5ac926fafcb
Peter Leonard <pleonard@El-Cap-15.wireless.yale.internal>
2016-03-14 16:15:07 -0400

    Working on about page

---
+++ simple-pages/page/show.php
@@ -0,0 +1,44 @@
+
+
+<?php echo head(array(
+    'title' => metadata('simple_pages_page', 'title'),
+    'bodyclass' => 'page simple-page',
+    'bodyid' => metadata('simple_pages_page', 'slug')
+)); ?>
+<style>
+
+
+.post-content{
+    padding-left:345px;
+}
+	</style>
+<div id="primary"> 
+
+
+
+  <div class="col-fixed">
+    .col-fixed
+  </div>
+  <div class="row">
+    <div class="col-md-3">
+      .col-md-3
+    </div>
+    <div class="col-md-3">
+      .col-md-3
+    </div>
+    <div class="col-md-3">
+      .col-md-3
+    </div>
+    <div class="col-md-3">
+      .col-md-3
+    </div>
+  </div>
+
+
+
+
+    
+
+</div>
+
+<?php echo foot(); ?>

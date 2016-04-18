f5d9458f97b08e8008410783b48cf6e9e755b6e2
Peter Leonard <peter.leonard@yale.edu>
2016-04-18 15:46:38 -0400

    Re-connected this page to the simple pages plugin

--- simple-pages/page/show.php
+++ simple-pages/page/show.php
@@ -10,67 +10,13 @@
 
 </style>
 
-<div id="primary"> 
-	<div class="container">
-<div class="row">
-	 <div class="col-md-6 col-md-offset-3">
-		 <h2>About the project</h2>
-	 </div>
-</div>
-<div class="row">
-  <div class="col-md-5 col-md-offset-1">Transcribe@Yale invites you to interact with Yale's digital collections by transcribing historic manuscripts. Our digital library holds hundreds of thousands of items &mdash; much more than library staff could ever catalog alone, so we're appealing to the public to help out by attaching text in the form of transcriptions, tags, and comments. By sharing your expertise on manuscripts that are in unusual scripts (such as Cherokee syllabary) or difficult to read, you are allowing a wider audience of researchers to benefit from our digital collections.</div>
-  <div class="col-md-5">Through "crowdsourcing," or engaging volunteers to contribute effort toward large-scale goals, these mass quantities of digitized artifacts become searchable, allowing researchers to quickly seek out specific information, and general users to browse and enjoy the materials more easily. Please join us in preserving and interpreting our past by keeping the historic record accessible &mdash; one page or picture at a time.</div>
-</div>
-<div class="row">
-	 <div class="col-md-6 col-md-offset-3">
-		 <h2>About the collections</h2>
-	 </div>
-</div>
-<div class="row">
-	<div class="col-md-5 col-md-offset-1">Content on Transcribe@Yale is drawn from Yale University's digital collections, featuring digitized selections from the Beinecke Rare Book & Manuscript Library. </div>
-	<div class="col-md-5">Transcribe@Yale will continuously be updated with additional content, so check back! If you would like to suggest items for inclusion from Yale's holdings, please contact us.</div>
-</div>
-<div class="row">
-	 <div class="col-md-6 col-md-offset-3">
-		 <h2>What is the goal of this project?</h2>
-	 </div>
-</div>
-<div class="row">
-	<div class="col-md-5 col-md-offset-1">The goal of Transcribe@Yale is to make historic documents more accessible – both by enhancing catalog records for greater ease in searching and browsing, and by engaging the public to interact with the materials in new ways. While typeset texts can be scanned with OCR (optical character recognition) technology to quickly and inexpensively add full text searchability, there’s no such easy fix for other primary source materials like handwritten documents or photographs. Making these items findable requires time-consuming manual labor to transcribe or describe each item – a process that doesn’t scale with traditional library workflows.</div>
-	<div class="col-md-5"> By outsourcing this work to volunteers, users can search on this added text to more quickly and easily find what they’re seeking. With Transcribe@Yale, we’re also hoping to attract new users interested in more active engagement with the collections. By volunteering their time to help make these materials more accessible, participants can learn new information about past eras while assisting others, including researchers using the documents, as well as the original authors and photographers whose stories they're helping to preserve.</div>
-	</div>
-
-<div class="row">
-	 <div class="col-md-6 col-md-offset-3">
-		 <h2>What technology powers the site?</h2>
-	 </div>
-</div>
-<div class="row">
-	<div class="col-md-5 col-md-offset-1">Transcribe@Yale is based on the University of Iowa's DIY History (Thank you, Iowa!); source code and documentation is available for institutions who wish to build a similar transcription project.</div>
-	<div class="col-md-5">The transcription pages use Omeka for content management, the Scripto plugin for transcribing, and Twitter Bootstrap for the frontend framework.</div>
-	</div>
-
-
-<div class="row">
-	 <div class="col-md-6 col-md-offset-3">
-		 <h2>Can anyone participate?</h2>
-	 </div>
-</div>
-<div class="row">
-	<div class="col-md-5 col-md-offset-1">Anyone is welcome to contribute to the site – no special expertise is required. </div>
-	<div class="col-md-5">To get started, you can create an account in order to track your contributions, and create a watchlist of your favorite pages.</div>
-</div>
-	
-
-	
-	
-	
-	
-	
-	
-</div>
-
-</div>
-</div>
+    <?php
+    $text = metadata('simple_pages_page', 'text', array('no_escape' => true));
+    if (metadata('simple_pages_page', 'use_tiny_mce')) {
+        echo $text;
+    } else {
+        echo eval('?>' . $text);
+    }
+    ?>
 
 <?php echo foot(); ?>

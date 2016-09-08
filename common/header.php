24acc519a21c7c94d72faa2d0dce94ccae1c288e
Peter Leonard <peter.leonard@yale.edu>
2016-09-08 12:26:05 -0400

    Minor adjustments for touch issues

--- common/header.php
+++ common/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php if ( $description = option('description')): ?>
         <meta name="description" content="<?php echo $description; ?>" />
     <?php endif; ?>
